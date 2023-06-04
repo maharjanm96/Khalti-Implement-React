41a719cb158d70955c8c71c1adb524c626e7b01b
Manish Maharjan <maharjanm96@gmail.com>
2023-06-04 22:09:38 +0545

    Khalti Test Implementation

--- src/components/Khalti/khaltiConfig.js
+++ src/components/Khalti/khaltiConfig.js
@@ -1,15 +1,34 @@
 import myKey from "./khaltiKey"
+// import axios from "axios";
 
 let config = {
     // replace this key with yours
     "publicKey": myKey.publicTestKey,
-    "productIdentity": "1234567890",
-    "productName": "Drogon",
+    "ticketIdentity": "GT-254X4Z",
+    "productName": "Umeet",
     "productUrl": "https://localhost:3000",
     "eventHandler": {
         onSuccess(payload) {
             // hit merchant api for initiating verfication
             console.log(payload);
+            alert ('Success!!!')
+            // let data = {
+            //     "token": payload.token,
+            //     "amount": payload.amount
+            // };
+
+            // let config = {
+            //     headers: { 'Authorization': myKey.secretKey }
+            // };
+
+            // axios.post("https://khalti.com/api/v2/payment/verify/", data, config)
+            //     .then(response => {
+            //         console.log(response.data);
+            //     })
+            //     .catch(error => {
+            //         console.log(error);
+            //     });
+
         },
         // onError handler is optional
         onError(error) {
